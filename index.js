4e38786045eeca222aeebc831ee2c14d21d09ef3
varun jain <varunjain.jain.j@gmail.com>
2026-02-05 14:49:36 +0530

    fix: removed nodemon from production and invoked the cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const connectDB = require('./config/db.js');
 const productRoutes = require('./routes/ProductRoute.js');
 const searchRoutes = require('./routes/SearchRoute.js');
+const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 8080
 app.use(express.json());
+app.use(cors())
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/search", searchRoutes);
 
